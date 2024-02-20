0ebca5fe12714486c7afe9262c02d5fc2210282c
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-20 14:42:20 +0100

    add base functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,31 +2,34 @@ const pricePerKm = 0.21;
 const kidsDiscount = 20;
 const eldersDiscount = 40;
 
+const inputName = document.querySelector('#passenger_name');
+const inputKm = document.querySelector('#passenger_km');
+const inputAge = document.querySelector('#age_range');
 const button = document.querySelector('button');
-const passengerName = document.querySelector('#passenger_name');
-const passengerKm = document.querySelector('#passenger_km');
-const passengerAge = document.querySelector('#age_range');
 
 button.addEventListener('click', () => {
-    console.log(passengerName.value, passengerAge.value, passengerKm.value);
+    const passengerName = inputName.value;
+    const passengerKm = Number(inputKm.value);
+    const passengerAge = inputAge.value;
+    let discountApplied;
+    // get name, kilometers and age
+    console.log(passengerName, passengerKm, passengerAge);
+    // get full trip price 
+    let tripPrice = passengerKm * pricePerKm;
+    console.log(`The final price is ${tripPrice}€`);
+    // subtract 20% from the full price if the user is a minor
+    if (passengerAge === 'minor') {
+        tripPrice -= tripPrice / 100 * kidsDiscount;
+        discountApplied = 'Kids Discount Applied';
+    } else if (passengerAge === 'senior') {
+        tripPrice -= tripPrice / 100 * eldersDiscount;
+        discountApplied = 'Elders Discount Applied';
+    } else {
+        discountApplied = 'Adult Price';
+    }
+    // ensure the final price has a maximum of two decimal places
+    const finalPrice = tripPrice.toFixed(2);
+    console.log(`The final price is ${finalPrice}€ -> ${discountApplied}`);
 })
 
-// get kilometers and age of the user
-// const userKm = Number(prompt('How many kilometers would you like to travel?'));
-// const userAge = Number(prompt('How old are you?'));
 
-// multiply user km by 0.21 € to get the full price
-// let tripPrice = userKm * pricePerKm;
-
-// subtract 20% from the full price if the user is a minor
-// if (userAge < 18) {
-//     tripPrice -= tripPrice / 100 * kidsDiscount;
-// }
-// subtract 40% from the full price if the user is more than 65
-// if (userAge >= 65) {
-//     tripPrice -= tripPrice / 100 * eldersDiscount;
-// }
-
-// ensure the final price has a maximum of two decimal places
-// const finalPrice = tripPrice.toFixed(2);
-// alert(`The final price is ${finalPrice}€`);
